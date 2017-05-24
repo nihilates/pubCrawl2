aaf326fe57dd8961d24fafa5f144d999f663fd72
nihilates <nihilates@gmail.com>
2017-05-24 13:12:14 -0400

    split components

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-
-class App extends React.Component {
-  render () {
-    return <p>Sup guy</p>;
-  }
-}
+import App from './components/app.jsx';
 
 render(<App/>, document.getElementById('app'));
