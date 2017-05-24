8ad4927858da36ffcd051f837e2df979f3177032
nihilates <nihilates@gmail.com>
2017-05-24 13:40:48 -0400

    Added bootstrap4 + JQuery

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,6 +3,26 @@ import {render} from 'react-dom';
 
 export default class App extends React.Component {
   render () {
-    return <p>Sup guys</p>;
+    return (
+      <div className="container">
+        <div id="topper" className="row">
+        top
+        </div>
+
+        <div id="middler" className="row">
+          <div className="col-md-6">
+          left
+          </div>
+
+          <div className="col-md-6">
+          right
+          </div>
+        </div>
+
+        <div id="footer" className="row">
+        bottom
+        </div>
+      </div>
+    )
   }
 };
