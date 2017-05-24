a5cfd5a9764b342773131b0f1700bca14c46fc1e
nihilates <nihilates@gmail.com>
2017-05-24 12:56:32 -0400

    added babel support and react framework

--- client/index.jsx
+++ client/index.jsx
@@ -1,1 +1,10 @@
-console.log("Pack it up pack it in");
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <p>Sup guy</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
